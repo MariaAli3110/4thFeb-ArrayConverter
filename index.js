8f5096211259b6b026359bf2756be51cb1ad9a39
MariaAli3110 <99141124+MariaAli3110@users.noreply.github.com>
2022-02-07 11:58:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+  function getVal(){
+    let input_csv =document.getElementById('input_screen').value;
+    document.getElementById('output_screen').value = '[' + input_csv +']';
+  }
+
+  
+
+    function Func(){let n = 5; // row or column count
+      // defining an empty string
+      let string = "";
+      
+      for(let i = 0; i < n; i++) { // external loop
+        for(let j = 0; j < n; j++) { // internal loop
+          if(i === 0 || i === n - 1) {
+            string += "*";
+          }
+          else {
+            if(j === 0 || j === n - 1) {
+              string += "*";
+            }
+            else {
+              string += " ";
+            }
+          }
+        }
+        // newline after each row
+        string += "\n";
+      }
+      // printing the string
+      console.log(string);}
